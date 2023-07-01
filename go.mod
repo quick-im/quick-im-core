f0abfa7c1b0e9e1593a9586e2c31876eaad07513
oldcat <924417424@qq.com>
2023-07-01 13:50:42 +0800

    添加日志工具

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 )
 
 require (
@@ -62,6 +64,7 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xtaci/kcp-go v5.4.20+incompatible // indirect
+	go.uber.org/zap v1.24.0
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230307190834-24139beb5833 // indirect
 	golang.org/x/mod v0.9.0 // indirect
---
+++ internal/logger/logger.go
@@ -0,0 +1,73 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// 日志文件名格式
+const logFileNameFormat = "2006-01-02.log"
+
+// 按日期记录日志的Hook
+type DateHook struct {
+	lock sync.Mutex
+	file *os.File
+	day  int
+}
+
+// 实现WriteSyncer接口
+func (h *DateHook) Write(p []byte) (n int, err error) {
+	// 获取当前日期
+	today := time.Now().Day()
+
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	// 如果日期发生变化，关闭旧文件，创建新文件
+	if today != h.day {
+		if h.file != nil {
+			h.file.Close()
+		}
+
+		// 创建新文件
+		filename := time.Now().Format(logFileNameFormat)
+		h.file, err = os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create log file: %v\n", err)
+			return 0, err
+		}
+
+		h.day = today
+	}
+
+	// 写入日志到文件
+	return h.file.Write(p)
+}
+
+func NewLogger(logPath string, logLevel zapcore.Level) (*zap.Logger, error) {
+	// 创建日志文件的Hook
+	hook := &DateHook{}
+
+	// 设置日志输出格式
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+
+	// 创建日志核心对象
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.AddSync(hook),
+		logLevel,
+	)
+
+	// 创建Logger对象
+	logger := zap.New(core, zap.AddCaller())
+
+	return logger, nil
+}
